112958ede0560d8340ace56751b32eb18cc3bc5d
Jasmin2208 <135834150+Jasmin2208@users.noreply.github.com>
2023-06-08 11:25:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const path = require("path")
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname,"./client/build")))
+app.use(express.static(path.join(__dirname,"/client/build")))
 
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/category", categoryRoutes)
