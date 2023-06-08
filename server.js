02e65fa4a14b4b98258cd96612cd67a41f6d30da
Jasmin2208 <jasminkorat204@gmail.com>
2023-06-08 10:23:24 +0530

    Manage Deployment

--- server.js
+++ server.js
@@ -8,18 +8,19 @@ const app = express()
 const cors = require("cors")
 const categoryRoutes = require("./routers/categoryRoutes")
 const productRoutes = require("./routers/productRoutes")
+const path = require("path")
 
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname,"./client/build")))
 
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/category", categoryRoutes)
 app.use("/api/v1/product", productRoutes)
 
-
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.use("*", function(req, res){
+  res.sendFile(path.join(__dirname,"./client/build/index.html"))
 })
 
 app.listen(PORT,()=>{
